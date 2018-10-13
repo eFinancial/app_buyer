5dcd8bf6da87b9602208c7afe228aa05a795811d
Tobi <tobias.reski@gmail.com>
2018-10-13 14:34:15 +0200

    Secwond try at fixing

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {NavController, ToastController} from 'ionic-angular';
 import {QRScanner, QRScannerStatus} from "@ionic-native/qr-scanner";
 import {InvoiceData} from "../../model/store-data.model";
 
@@ -49,11 +49,71 @@ export class HomePage {
     }
   };
 
-  constructor(public navCtrl: NavController, private qrScanner: QRScanner){
+  constructor(public navCtrl: NavController,
+              private qrScanner: QRScanner,
+              private toastCtrl: ToastController){
     this.scanQrCode();
   }
 
-  scanQrCode(){
+  ionViewDidEnter() {
+    this.qrScanner.prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          // camera permission was granted
+
+          let camtoast = this.toastCtrl.create({
+            message: 'camera permission granted',
+            duration: 1000
+          });
+          camtoast.present();
+          // start scanning
+
+          this.qrScanner.show()
+          window.document.querySelector('ion-app').classList.add('cameraView');
+
+          let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+
+            console.log('Scanned something', text);
+            this.qrScanner.hide(); // hide camera preview
+            window.document.querySelector('ion-app').classList.remove('cameraView');
+
+            const toast = this.toastCtrl.create({
+              message: 'You scanned text is this :' + text,
+              duration: 6000
+            });
+            toast.present();
+            scanSub.unsubscribe(); // stop scanning
+          });
+
+
+        } else if (status.denied) {
+          const toast = this.toastCtrl.create({
+            message: 'camera permission was denied',
+            duration: 3000
+          });
+          toast.present();
+          // camera permission was permanently denied
+          // you must use QRScanner.openSettings() method to guide the user to the settings page
+          // then they can grant the permission from there
+        } else {
+          const toast = this.toastCtrl.create({
+            message: 'You can ask for permission again at a later time.',
+            duration: 3000
+          });
+          toast.present();
+          // permission was denied, but not permanently. You can ask for permission again at a later time.
+        }
+      })
+      .catch((e: any) => console.log('Error is', e));
+  }
+
+  ionViewWillLeave(){
+
+    window.document.querySelector('ion-app').classList.remove('cameraView');
+
+  }
+
+    scanQrCode(){
     this.qrScanner.prepare()
       .then((status: QRScannerStatus) => {
         if (status.authorized) {
